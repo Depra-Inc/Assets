4b5d30c1bf3443a4fa768ffb4cb21db68e43f51e
godzzz <g0dzZz1lla@yandex.ru>
2023-07-09 01:02:44 +0400

    System.IO support

---
+++ Assets.IO/DirectoryInfoExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Assets.IO
+{
+    public static class DirectoryInfoExtensions
+    {
+        public static bool IsEmpty(this DirectoryInfo directoryInfo) =>
+            directoryInfo.EnumerateFileSystemInfos().Any() == false;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static DirectoryInfo CreateIfNotExists(this DirectoryInfo directoryInfo)
+        {
+            if (directoryInfo.Exists == false)
+            {
+                directoryInfo.Create();
+            }
+
+            return directoryInfo;
+        }
+    }
+}
---
+++ Assets.IO/FileSystemAssetIdent.cs
@@ -0,0 +1,59 @@
+﻿// Copyright © 2023 Nikolay Melnikov. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System.IO;
+using Depra.Assets.Idents;
+
+namespace Assets.IO
+{
+    public sealed class FileSystemAssetIdent : IAssetIdent
+    {
+        internal readonly FileInfo SystemInfo;
+
+        public static FileSystemAssetIdent Empty => new(string.Empty);
+        public static FileSystemAssetIdent Invalid => new(nameof(Invalid));
+
+        public FileSystemAssetIdent(FileInfo systemInfo, string relativePath = "")
+        {
+            SystemInfo = systemInfo;
+            RelativePath = relativePath;
+            SystemInfo.Directory.CreateIfNotExists();
+            Name = SystemInfo.Name.Replace(Extension, string.Empty);
+        }
+
+        public FileSystemAssetIdent(string path, string relativePath = "")
+            : this(new FileInfo(path), relativePath) { }
+
+        public FileSystemAssetIdent(string nameWithExtension, string directory, string relativePath = "")
+            : this(Path.Combine(directory, nameWithExtension), relativePath) { }
+
+        public FileSystemAssetIdent(string name, string directory, string extension = null, string relativePath = null)
+            : this(Path.Combine(directory, name + extension), relativePath) { }
+
+        public string Name { get; }
+
+        public string Extension => SystemInfo.Extension;
+        
+        public string AbsolutePath => SystemInfo.FullName;
+        
+        public string NameWithExtension => Name + Extension;
+
+        public string RelativePath { get; }
+
+        public string AbsoluteDirectoryPath => SystemInfo.DirectoryName;
+
+        public string RelativeDirectoryPath => SystemInfo.Directory!.Name;
+
+        internal void ThrowIfNotExists()
+        {
+            if (SystemInfo.Exists == false)
+            {
+                throw new FileNotFoundException($"File not found at path: {AbsolutePath}");
+            }
+        }
+
+        string IAssetIdent.Uri => AbsolutePath;
+
+        string IAssetIdent.RelativeUri => RelativePath;
+    }
+}
